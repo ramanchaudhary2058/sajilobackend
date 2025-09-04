23534f9a56110deaf3ab99f7798d0c29a2f86e96
raman <ramancdry2058@gmail.com>
2025-09-04 19:14:09 +0545

    'adde'd

--- server.ts
+++ server.ts
@@ -13,8 +13,6 @@ import {
 } from "./routes/";
 
 import { app, serverInstance } from "./real-time/RealTime";
-
-
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
